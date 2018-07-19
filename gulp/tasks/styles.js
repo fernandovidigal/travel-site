12158dc435551616874c167c1bbb2e600f370264
Fernando <fernandovidigal@gmail.com>
2018-07-19 17:10:30 +0100

    Inicio de Mobil-First Approach, Adição de Mixins

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,12 +1,17 @@
 var gulp = require('gulp');
 var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer'); 
-var cssvars = require('postcss-simple-vars');
+var cssVars = require('postcss-simple-vars');
 var nested = require('postcss-nested');
 var cssImport = require('postcss-import');
+var mixins = require('postcss-mixins');
 
 gulp.task('styles', function(){
     return gulp.src('./app/assets/styles/styles.css')
-            .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
-            .pipe(gulp.dest('./app/temp/styles'));
+        .pipe(postcss([cssImport, mixins, cssVars, nested, autoprefixer]))
+        .on('error', function(errorInfo){
+            console.log(errorInfo.toString());
+            this.emit('end');
+        })
+        .pipe(gulp.dest('./app/temp/styles'));
 });
